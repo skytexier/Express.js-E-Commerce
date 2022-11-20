478d9697b7ab84b130493670e5e623e3bb42f5d1
Sky Texier <skyhamilton@gmail.com>
2022-11-20 09:25:06 -0800

    Updating tag routes
    Lot of copy and pasting the same queries for update, destroy and  even create.. Most routes for these tags are very similar considering we are working
    by an id param

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,27 +2,58 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+router.get('/', (req, res) => {
+  Tag.findAll({
+    include : [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+    }).then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
+  });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+    Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include : [
+        {
+          model: Product,
+          through: ProductTag,
+        },
+      ],
+      }).then((data) => res.status(200).json(data))
+      .catch((err) => res.status(500).json(err));
+    });
 
 router.post('/', (req, res) => {
-  // create a new tag
-});
+  Tag.create({
+    tag_name: req.body,
+    }).then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
+  });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+  Tag.update({
+    where: {
+      id: req.params.id,
+    },
+    }).then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
+  });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+    }).then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
+  });
 
 module.exports = router;
